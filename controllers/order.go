8cb55cfbfce442fc154e80ced40cb85ec62da80c
Initcc01 <c_xiangcs02@163.com>
2024-05-06 22:34:21 +0800

    feature:更新order

--- controllers/order.go
+++ controllers/order.go
@@ -4,8 +4,19 @@ import "github.com/gin-gonic/gin"
 
 type OrderController struct{}
 
+type Search struct {
+	Name string `json:"name"`
+	Cid  int    `json:"cid"`
+}
+
 func (o OrderController) GetList(ctx *gin.Context) {
-	cid := ctx.PostForm("cid")
-	name := ctx.DefaultPostForm("name", "wangwu") //空值时会自动赋一个默认值
-	ReturnSuccess(ctx, 0, cid, name, 1)
+	//cid := ctx.PostForm("cid")
+	//name := ctx.DefaultPostForm("name", "wangwu") //空值时会自动赋一个默认值
+	//ReturnSuccess(ctx, 0, cid, name, 1)
+
+	param := make(map[string]any)
+	err := ctx.BindJSON(&param)
+	if err == nil {
+
+	}
 }
